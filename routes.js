52de9b37d7f75edbf5f6ad074e916f07b8c647cb
Kevin Eddye <kevneddy@gmail.com>
2016-11-27 21:30:22 -0800

    Add event listener and data store

--- routes.js
+++ routes.js
@@ -2,87 +2,27 @@
 import React, { Component } from 'react'
 import { Route, IndexRoute, Link } from 'react-router'
 
+// Store
+import AppStore from './stores/AppStore'
+
 // Main component
-class App extends Component {
-	componentDidMount(){
-		document.body.className=''
-	}
-	render(){
-		return (
-			<div>
-				<h1>React Universal Blog</h1>
-					<nav>
-						<ul>
-							<li><Link to="/">Home</Link></li>
-							<li><Link to="/about">About</Link></li>
-							<li><Link to="/exercises">Exercises</Link></li>
-							<li><Link to="/contact">Contact</Link></li>
-						</ul>
-					</nav>
-				{ this.props.children }
-			</div>
-		)
-	}
-}
+imprt App from './components/App'
+
 
 // Pages
-class Home extends Component {
-	render(){
-		return (
-			<div>
-				<h2>Home</h2>
-				<div>Some home page content</div>
-			</div>
-		)
-	}
-}
-class About extends Component {
-	render(){
-		return (
-			<div>
-				<h2>About</h2>
-				<div>Some about page content</div>
-			</div>
-		)
-	}
-}
-class Exercises extends Component {
-	render(){
-		return (
-			<div>
-				<h2>Exercises</h2>
-				<div>Some exercises page content</div>
-			</div>
-		)
-	}
-}
-class Contact extends Component {
-	render(){
-		return (
-			<div>
-				<h2>Contact</h2>
-				<div>Some contact page content</div>
-			</div>
-		)
-	}
-}
-class NoMatch extends Component {
-	render(){
-		return (
-			<div>
-				<h2>NoMatch</h2>
-				<div>404 error</div>
-			</div>
-		)
-	}
-}
+import Blog from './components/Pages/Blog'
+import Default from './components/Pages/Default'
+import Exercises from './components/Pages/Exercises'
+import NoMatch from '//components/Pages/NoMatch'
 
 export default (
-	<Route path='/' component={App}>
+	<Route path='/' data={AppStore.data} component={App}>
 		<IndexRoute component={Home}/>
 		<Route path="about" component={About}/>
 		<Route path="exercises" component={Exercises}/>
 		<Route path="contact" component={Contact}/>
+		<Route path="/exercises/:slug" component={Exercises}/>
+		<Route path="/blog/:slug" component={Blog}/>
 		<Route path="*" component={NoMatch}/>
 		</Route>
 )
