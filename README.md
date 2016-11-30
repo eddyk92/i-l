811401679f9dddc6cb51d4d4ba1b7805d8ef640d
Kevin Eddye <kevneddy@gmail.com>
2016-11-30 14:57:32 -0800

    Add routes and packagejson

---
+++ README.md
@@ -0,0 +1,10 @@
+#### A blog using React and CosmicJS 
+
+It uses the following:
+<br>
+1. [React](http://facebook.github.io/react/) for UI views<br>
+2. [Express](http://expressjs.com/) for server side rendering<br>
+3. [React Router](https://github.com/rackt/react-router) for routing<br>
+4. [React Hot Loader](https://github.com/gaearon/react-hot-loader) for hot loading in development<br>
+5. [Flux](https://facebook.github.io/flux/) for data flow<br>
+6. [Cosmic JS](https://cosmicjs.com) for content management
---
+++ routes.js
@@ -0,0 +1,88 @@
+// routes.js
+import React, { Component } from 'react'
+import { Route, IndexRoute, Link } from 'react-router'
+
+// Main component
+class App extends Component {
+  componentDidMount(){
+    document.body.className=''
+  }
+  render(){
+    return (
+      <div>
+        <h1>Invincible Leaders</h1>
+           <nav>
+              <ul>
+                 <li><Link to="/">Home</Link></li>
+                 <li><Link to="/about">About</Link></li>
+                 <li><Link to="/exercises">Exercises</Link></li>
+                 <li><Link to="/contact">Contact</Link></li>
+              </ul>
+           </nav>
+        { this.props.children }
+      </div>
+    )
+  }
+}
+
+// Pages
+class Home extends Component {
+  render(){
+    return (
+      <div>
+        <h2>Home</h2>
+        <div>Some home page content</div>
+      </div>
+    )
+  }
+}
+class About extends Component {
+  render(){
+    return (
+      <div>
+        <h2>About</h2>
+        <div>Some about page content</div>
+      </div>
+    )
+  }
+}
+class Exercises extends Component {
+  render(){
+    return (
+      <div>
+        <h2>Exercises</h2>
+        <div>Some exercise page content</div>
+      </div>
+    )
+  }
+}
+class Contact extends Component {
+  render(){
+    return (
+      <div>
+        <h2>Contact</h2>
+        <div>Some contact page content</div>
+      </div>
+    )
+  }
+}
+class NoMatch extends Component {
+  render(){
+    return (
+      <div>
+        <h2>NoMatch</h2>
+        <div>404 error</div>
+      </div>
+    )
+  }
+}
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={Home}/>
+    <Route path="about" component={About}/>
+    <Route path="exercises" component={Exercises}/>
+    <Route path="contact" component={Contact}/>
+    <Route path="*" component={NoMatch}/>
+  </Route>
+)
