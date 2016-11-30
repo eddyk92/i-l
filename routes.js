5c97fe86ee5f424fa52d811fe51afbff47749349
Kevin Eddye <kevneddy@gmail.com>
2016-11-30 15:07:44 -0800

    Broke pages up inot seperate components in routesjs

--- routes.js
+++ routes.js
@@ -1,81 +1,18 @@
 // routes.js
-import React, { Component } from 'react'
-import { Route, IndexRoute, Link } from 'react-router'
+import React from 'react'
+import { Route, IndexRoute } from 'react-router'
+
+// Store
+import AppStore from './stores/AppStore'
 
 // Main component
-class App extends Component {
-  componentDidMount(){
-    document.body.className=''
-  }
-  render(){
-    return (
-      <div>
-        <h1>Invincible Leaders</h1>
-           <nav>
-              <ul>
-                 <li><Link to="/">Home</Link></li>
-                 <li><Link to="/about">About</Link></li>
-                 <li><Link to="/exercises">Exercises</Link></li>
-                 <li><Link to="/contact">Contact</Link></li>
-              </ul>
-           </nav>
-        { this.props.children }
-      </div>
-    )
-  }
-}
+import App from './components/App'
 
 // Pages
-class Home extends Component {
-  render(){
-    return (
-      <div>
-        <h2>Home</h2>
-        <div>Some home page content</div>
-      </div>
-    )
-  }
-}
-class About extends Component {
-  render(){
-    return (
-      <div>
-        <h2>About</h2>
-        <div>Some about page content</div>
-      </div>
-    )
-  }
-}
-class Exercises extends Component {
-  render(){
-    return (
-      <div>
-        <h2>Exercises</h2>
-        <div>Some exercise page content</div>
-      </div>
-    )
-  }
-}
-class Contact extends Component {
-  render(){
-    return (
-      <div>
-        <h2>Contact</h2>
-        <div>Some contact page content</div>
-      </div>
-    )
-  }
-}
-class NoMatch extends Component {
-  render(){
-    return (
-      <div>
-        <h2>NoMatch</h2>
-        <div>404 error</div>
-      </div>
-    )
-  }
-}
+import Blog from './components/Pages/Blog'
+import Default from './components/Pages/Default'
+import Exercises from './components/Pages/Exercises'
+import NoMatch from './components/Pages/NoMatch'
 
 export default (
   <Route path="/" component={App}>
